28dd1516a9c35462f734f570dd50fb8d0f4199f9
User <genik-vlad@mail.ru>
2022-07-03 19:14:57 +0300

    Добавьте файлы проекта.

---
+++ Practical_4_8_3_ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Practical_4_8_3_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Угадай число");
+            Console.WriteLine(" Введите верхнюю границу диапазона для поиска");
+            int n = int.Parse(Console.ReadLine());
+            Random r = new Random();
+            int goal = r.Next(n);
+            string str;
+            while (true)
+            {
+                Console.WriteLine(" Введите число,если устали нажмите Enter");
+                str = Console.ReadLine();
+                if (str == string.Empty)
+                {
+                    Console.WriteLine(" Вы сдались. Это число {0}", goal);
+                    break;
+                }
+                if (int.Parse(str) == goal)
+                {
+                    Console.WriteLine(" Вы угадали ", goal);
+                    break;
+                }
+                else if (int.Parse(str) > goal)
+                {
+                    Console.WriteLine(" Ваше число больше");
+                }
+
+                else
+                {
+                    Console.WriteLine(" Ваше число меньше");
+                }
+
+
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
